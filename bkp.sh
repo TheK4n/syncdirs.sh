78732385cbda5b87a767d77104bdb128e2826b3c
TheK4n <djvlad967891@gmail.com>
2022-02-19 17:13:47 +0300

    many changes

--- bkp.sh
+++ bkp.sh
@@ -5,115 +5,127 @@ BACKUP_DIR_2="$BACKUP_DIR"/2
 MEDIA_BACKUP="/media/backup"
 GPG_ID="$BACKUP_DIR"/.gpg-id
 
+BACKUP_FILE="$BACKUP_DIR/backup.conf"
+LOG_FILE="$BACKUP_DIR/backup.log"
+
+SCRIPT_NAME="$(basename "$0")"
 
 
 die() {
-	echo "$@" >&2
+	echo "$SCRIPT_NAME: Error: $1" >&2
 	exit 1
+}
+
+log_msg() {
+    echo "$SCRIPT_NAME: $(date +"%d-%m-%Y %T"): $1" >> "$LOG_FILE"
+}
+
+log_error() {
+    log_msg ": Error: $1"
 }
 
 cmd_init() {
 
-    test -z "$(gpg -k | grep "$1")" && die "No public key $1"
-    test -z "$(gpg -K | grep "$1")" && die "No private key $1"
-
+    test -z "$(gpg -k | grep "$1")" && die "No public key '$1'"
+    test -z "$(gpg -K | grep "$1")" && die "No private key '$1'"
 
     mkdir -p "$BACKUP_DIR"/1 || true
-    ln -s $MEDIA_BACKUP "$BACKUP_DIR_2" || die "not mounted $MEDIA_BACKUP"
+    touch "$LOG_FILE"
+    touch "$BACKUP_FILE"
+    ln -s $MEDIA_BACKUP "$BACKUP_DIR_2" || die "not mounted '$MEDIA_BACKUP'"
     echo "$1" > "$GPG_ID"
 }
 
-rsync_all() {
+cmd_rsync_all() {
     rsync -ra "$BACKUP_DIR_1"/* "$BACKUP_DIR_2" && rsync -ra "$BACKUP_DIR_2"/* "$BACKUP_DIR_1"
 }
 
 cmd_insert() {
 
-    [[ "${1:0:1}" == [/~.] ]] && die "you cant use absolute path"
-    
-    [[ -e "$BACKUP_DIR_1"/"$1" ]] && die "already exists"
-    [[ -e "$BACKUP_DIR_2"/"$1" ]] && die "already exists"
-    [[ -e "$BACKUP_DIR_1"/"$1".bkp ]] && die "already exists"
-    [[ -e "$BACKUP_DIR_2"/"$1".bkp ]] && die "already exists"
+    test -d "$1" && die "only files, not directory"
 
-    local enc_file_name
-    mkdir -p "$BACKUP_DIR_1"/"$(dirname $1)" && echo "created directory: $BACKUP_DIR_1/$(dirname $1)"
-    enc_file_name="$BACKUP_DIR_1"/"$(dirname $1)"/"$(basename $1)".bkp
+    test -f "$BACKUP_DIR_1/$(basename "$1")" && die "'$(basename "$1")' already exists"
 
-    if [ -d "$1" ]
-    then
-        tar czf - "$1"/* | gpg -e -R "$(cat $GPG_ID)" > "$enc_file_name"
-    else
-        tar czf - "$1" | gpg -e -R "$(cat $GPG_ID)" > "$enc_file_name"
-    fi
-
-    rsync_all
+    file_name="$(basename "$1")"
+    gpg -e -R "$(cat "$GPG_ID")" -o "$BACKUP_DIR_1"/"$file_name" "$1"
+    cmd_rsync_all
 }
 
 cmd_delete() {
-   
-    if [ -d "$BACKUP_DIR_1"/"$1" ]
-    then
-        rm -r "$BACKUP_DIR_1"/"$1"
-    else
-        rm "$BACKUP_DIR_1"/"$1".bkp
-    fi
-    
-    if [ -d "$BACKUP_DIR_2"/"$1" ]
-    then
-        rm -r "$BACKUP_DIR_2"/"$1"
-    else
-        rm "$BACKUP_DIR_2"/"$1".bkp
-    fi
+    file_name="$(basename "$1")"
+    rm "$BACKUP_DIR_1"/"$file_name"
+    rm "$BACKUP_DIR_2"/"$file_name" && \
+    echo "$SCRIPT_NAME: '$BACKUP_DIR_1/$file_name' Removed"
 }
-
 
 cmd_show() {
     echo "Backup"
-    tree "$BACKUP_DIR_1"
+    tree "$BACKUP_DIR_1" | tail -n +2
 }
 
-
 cmd_restore() {
-
-    if [ -d "$BACKUP_DIR_1"/"$1" ]
-    then
-        echo no
-    else
-        filename_="$(basename "$1")"
-        filename_="${filename%.*}"
-        echo $filename_
-        mkdir -p "$(dirname "$1")"
-        gpg -d "$BACKUP_DIR_1"/"$1".bkp | tar xzf - | "$(dirname "$1")"/"$filename_"
-    fi
-
+    file_name=$(basename "$1")
+    test -e "$BACKUP_DIR_1"/"$1" && gpg -d "$BACKUP_DIR_1"/"$1" > "$file_name"
 }
 
 cmd_diskusage() {
-    if [ -d "$BACKUP_DIR_1"/"$1" ]
-    then
-        du -hs "$BACKUP_DIR_1"/"$1"
-        du -hs "$BACKUP_DIR_2"/"$1"
-    else
-        du -hs "$BACKUP_DIR_1"/"$1".bkp
-        du -hs "$BACKUP_DIR_2"/"$1".bkp
-    fi
+    du -hs "$BACKUP_DIR_1"/"$1"
+    du -hs "$BACKUP_DIR_2"/"$1"
+}
 
+cmd_register() {
+    if [ -f "$1" ]; then
+        realpath "$1" >> "$BACKUP_FILE"
+        log_msg "Register '$(realpath "$1")'"
+    else
+        log_error "'$1' not a file"
+    fi
+}
+
+cmd_registered() {
+    cat "$BACKUP_FILE"
+}
+
+cmd_cron() {
+    for i in $(cat "$BACKUP_FILE" | tr '\n' ' ');
+    do
+        if [ -f "$i" ] 
+        then
+            cmd_insert "$i"
+            log_msg "Backup '$i'"
+        else
+            log_error "'$i' not exists"
+        fi
+    done
+}
+
+cmd_regedit() {
+    $EDITOR "$BACKUP_FILE"
+}
+
+cmd_log() {
+    less "$LOG_FILE"
 }
 
 
 case "$1" in
-	init) shift;			    cmd_init    "$@" ;;
-	help|--help) shift;		    cmd_usage   "$@" ;;
-	version|--version) shift;	cmd_version "$@" ;;
-	show|ls|list) shift;		cmd_show    "$@" ;;
-	find|search) shift;		    cmd_find    "$@" ;;
-	grep) shift;			    cmd_grep    "$@" ;;
-	insert|add) shift;		    cmd_insert  "$@" ;;
-    sync) shift;                rsync_all   "$@" ;;
-    restore) shift;             cmd_restore "$@" ;;
-	delete|rm|remove) shift;	cmd_delete  "$@" ;;
+	init) shift;			          cmd_init    "$@" ;;
+	#help|--help) shift;		     cmd_usage   "$@" ;;
+	#version|--version) shift;	 cmd_version "$@" ;;
+	show|ls|list) shift;		    cmd_show    "$@" ;;
+	#find|search) shift;		     cmd_find    "$@" ;;
+	#grep) shift;			           cmd_grep    "$@" ;;
+	insert|add) shift;		      cmd_insert  "$@" ;;
+  sync) shift;                cmd_rsync_all   "$@" ;;
+  restore) shift;             cmd_restore "$@" ;;
+	delete|rm|remove) shift;   	cmd_delete  "$@" ;;
 	du) shift;	                cmd_diskusage  "$@" ;;
-	*)				            cmd_show    "$@" ;;
+  register|reg) shift;        cmd_register "$@" ;;
+  registered) shift;          cmd_registered "$@" ;;
+  regedit) shift;             cmd_regedit "$@" ;;
+  cron) shift;                cmd_cron     "$@" ;;
+  log) shift;                 cmd_log      "$@" ;;
+
+	*)				                  cmd_show    "$@" ;;
 esac
 exit 0
